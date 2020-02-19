4294e2ff6fbf46b2046e1d93a090e59317cacd54
ClassUser <serswii@yandex.ru>
2020-02-19 11:38:23 +0300

    286-SmirnovID

---
+++ PHP/lesson7/index.php
@@ -0,0 +1,15 @@
+<div align="center">
+	<form method="post" action="apps.php" enctype="multipart/form-data">
+		Имя<br>
+		<input type="text" name="name"><br><br>
+		Пароль<br>
+		<input type="password" name="password"><br><br>
+		e-mail<br>
+		<input type="email" name="email"><br><br>
+		Загрузите файл<br>
+		<input type="file" name="upfile"><br><br>
+		Дата рождения<br>
+		<input type="date" name="bdate"><br><br>
+		<input type="submit" value="Отправить">
+	</form>
+</div>
