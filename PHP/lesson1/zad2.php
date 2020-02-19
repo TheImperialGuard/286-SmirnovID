8ef44bafc6a0d95e8ef401f9c42590567b80c34c
Ivan Smirnov <61226003+TheImperialGuard@users.noreply.github.com>
2020-02-19 11:51:08 +0300

    Delete zad2.php

--- PHP/lesson1/zad2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$str = 'Бережков Андрей Вячеславоич'; 
-$str2 = explode(' ', $str);
-$a = substr($str, 17, 2);
-$b = substr($str, $start = 30, $length = 2);
-echo $str = $str2[0],' ' , $a,'.' , $b;
-?>
